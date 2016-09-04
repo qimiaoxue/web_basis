ce21d160cbb447f5b0d973308e3e7a71b5881d6b
xue yan <15011272359@163.com>
2016-09-04 20:41:45 +0800

    add client download web page

--- 1client.py
+++ 1client.py
@@ -1,14 +1,23 @@
-import socket 
+import socket
 s = socket.socket()
-host = 'g.cn'
+host = 'www.thebeijinger.com'
 port = 80
 s.connect((host, port))
 ip, port = s.getsockname()
 print '{} and {}'.format(ip, port)
 http_request = """GET / HTTP/1.1
 host: {}
+Connection: close
 
 """.format(host)
 s.send(http_request.encode('utf-8'))
-response = s.recv(1023)
-print response.decode('utf-8')
+response = b''
+while True:
+    r = s.recv(1023)
+    if len(r) == 0:
+        break
+    response += r
+    print 'hello jay'
+with open('beijinger.response.txt','wb') as f:
+    f.write(response)
+
