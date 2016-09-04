1a077e2552c12df62073ae700a5cac93a47e64f4
xue yan <15011272359@163.com>
2016-09-04 21:51:45 +0800

    add response doge img in server

--- 1server.py
+++ 1server.py
@@ -1,4 +1,8 @@
 import socket
+def read_from_file(filename):
+    with open(filename, 'rb') as f:
+       return f.read()
+
 host = ''
 port = 2000
 s = socket.socket()
@@ -8,8 +12,22 @@ while True:
     s.listen(3)
     connection, ip = s.accept()
     request = connection.recv(1023)
-    print request
-    response = b'hello world'
-    print response
+    print '{}'.format(request)
+    line = request.split('\r\n')[0]
+    path = line.split()[1]
+    print '{}'.format(path)
+    normal_response = b'hello world'
+    doge_response = b'''HTTP/1.1 200 OK
+Content-Type: text/html
+
+hello doge!<img src="doge.jpg"><img src="doge1.gif">'''
+    if path =='/doge':
+        response = doge_response
+    elif path == '/doge.jpg':
+        response = read_from_file("doge.jpg")
+    elif path == '/doge1.gif':
+        response = read_from_file("doge1.gif")
+    else:
+        response = normal_response
     connection.sendall(response)
     connection.close()
